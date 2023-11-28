03672f6bffd68cce6ea864cc104e461dbb688705
Pavel Zamulko <johanas.azzaid@gmail.com>
2023-11-28 18:45:55 +0100

    Module 2 Components: initial commit

--- src/app/modules/home/components/short-course-details/short-course-details.component.ts
+++ src/app/modules/home/components/short-course-details/short-course-details.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {Course} from "../../../../core/interfaces/Course";
+import {CommonModule} from "@angular/common";
 
 @Component({
+  standalone: true,
   selector: 'app-short-course-details',
   templateUrl: './short-course-details.component.html',
-  styleUrls: ['./short-course-details.component.css']
+  styleUrls: ['./short-course-details.component.css'],
+  imports: [CommonModule]
 })
 export class ShortCourseDetailsComponent {
+  @Input() course : Course | null = null;
 
 }
